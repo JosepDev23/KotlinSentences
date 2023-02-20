8d872284652882d3faaa4450236c3268420c27ad
josep <josep.ramon@dialapplet.com>
2023-02-20 16:19:05 +0100

    Ejercicio 1

--- app/src/main/java/dadm/jramrib/kotlinsentences/ui/MainActivity.kt
+++ app/src/main/java/dadm/jramrib/kotlinsentences/ui/MainActivity.kt
@@ -1,11 +1,16 @@
-package dadm.jramrib.kotlinsentences
+package dadm.jramrib.kotlinsentences.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dadm.jramrib.kotlinsentences.R
+import dadm.jramrib.kotlinsentences.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
