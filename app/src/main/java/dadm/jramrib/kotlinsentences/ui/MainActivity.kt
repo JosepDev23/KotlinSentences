dab0d6ea6dfc5f840bd0ce0394da21d0418e55d9
josep <josep.ramon@dialapplet.com>
2023-02-21 16:16:58 +0100

    Ejercicio 4

--- app/src/main/java/dadm/jramrib/kotlinsentences/ui/MainActivity.kt
+++ app/src/main/java/dadm/jramrib/kotlinsentences/ui/MainActivity.kt
@@ -3,6 +3,8 @@ package dadm.jramrib.kotlinsentences.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import dadm.jramrib.kotlinsentences.R
 import dadm.jramrib.kotlinsentences.databinding.ActivityMainBinding
@@ -15,6 +17,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         val navController = binding.fragmentContainerView.getFragment<NavHostFragment>().navController
         binding.bottomNavigationView.setupWithNavController(navController)
+
+        setSupportActionBar(binding.materialToolBar)
+
+        var appBarConfiguration = AppBarConfiguration(setOf(R.id.newQuotationFragment, R.id.favouritesFragment, R.id.settingsFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
 
