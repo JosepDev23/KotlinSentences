269127c993f97541a1dc56a34c69a7dbd27cdd47
josep <josep.ramon@dialapplet.com>
2023-03-22 19:05:56 +0100

    Ejercicio 10

--- app/src/main/java/dadm/jramrib/kotlinsentences/ui/newquotation/NewQuotationFragment.kt
+++ app/src/main/java/dadm/jramrib/kotlinsentences/ui/newquotation/NewQuotationFragment.kt
@@ -3,6 +3,7 @@ package dadm.jramrib.kotlinsentences.ui.newquotation
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import dadm.jramrib.kotlinsentences.R
 import dadm.jramrib.kotlinsentences.databinding.FragmentNewQuotationBinding
 
@@ -11,9 +12,15 @@ class NewQuotationFragment: Fragment(R.layout.fragment_new_quotation){
     private val binding
         get() = _binding!!
 
+    private val viewModel: NewQuotationViewModel by viewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentNewQuotationBinding.bind(view)
+
+        viewModel.userName.observe(viewLifecycleOwner) {
+            binding.tvGreetings.text = getString(R.string.greetings, it)
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/dadm/jramrib/kotlinsentences/ui/newquotation/NewQuotationViewModel.kt
+++ app/src/main/java/dadm/jramrib/kotlinsentences/ui/newquotation/NewQuotationViewModel.kt
@@ -1,4 +1,15 @@
 package dadm.jramrib.kotlinsentences.ui.newquotation
 
-class NewQuotationViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NewQuotationViewModel: ViewModel() {
+    private val _mutableUserName = MutableLiveData(getUserName())
+    val userName: LiveData<String>
+        get() = _mutableUserName
+
+    private fun getUserName(): String {
+        return setOf("Alice", "Bob", "Charlie", "David", "Emma").random()
+    }
 }
