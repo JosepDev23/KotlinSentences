4195d377e4bc7f86a5418621f4a65ad50ae97a33
JosepDev23 <josepdev23@gmail.com>
2023-03-26 21:41:40 +0200

    P3 E1

--- app/src/main/java/dadm/jramrib/kotlinsentences/ui/settings/SettingsFragment.kt
+++ app/src/main/java/dadm/jramrib/kotlinsentences/ui/settings/SettingsFragment.kt
@@ -3,22 +3,13 @@ package dadm.jramrib.kotlinsentences.ui.settings
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
 import dadm.jramrib.kotlinsentences.R
 import dadm.jramrib.kotlinsentences.databinding.FragmentNewQuotationBinding
 import dadm.jramrib.kotlinsentences.databinding.FragmentSettingsBinding
 
-class SettingsFragment:  Fragment(R.layout.fragment_settings){
-    private var _binding: FragmentSettingsBinding? = null
-    private val binding
-        get() = _binding!!
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        _binding = FragmentSettingsBinding.bind(view)
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+class SettingsFragment: PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences_settings, rootKey)
     }
 }
